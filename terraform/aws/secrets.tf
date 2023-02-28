fc0b225fc7345a9deb1aea9c78f5076a7591c699
Antonio Irizarry <55394816+airizarryDB@users.noreply.github.com>
2023-02-28 12:25:40 -0500

    Update secrets.tf

--- terraform/aws/secrets.tf
+++ terraform/aws/secrets.tf
@@ -8,18 +8,6 @@ resource "databricks_secret" "user-email" {
   scope        = databricks_secret_scope.sat.id
 }
 
-resource "databricks_secret" "user" {
-  key          = "user"
-  string_value = var.account_user
-  scope        = databricks_secret_scope.sat.id
-}
-
-resource "databricks_secret" "pass" {
-  key          = "pass"
-  string_value = var.account_pass
-  scope        = databricks_secret_scope.sat.id
-}
-
 resource "databricks_secret" "pat" {
   key          = "sat-token-${var.workspace_id}"
   string_value = var.workspace_PAT
@@ -34,6 +22,46 @@ resource "databricks_secret" "account_console_id" {
 
 resource "databricks_secret" "sql_warehouse_id" {
   key          = "sql-warehouse-id"
-  string_value = databricks_sql_endpoint.this.id
+  string_value = var.sqlw_id == "new" ? databricks_sql_endpoint.new[0].id : data.databricks_sql_warehouse.old[0].id
   scope        = databricks_secret_scope.sat.id
 }
+
+### AWS Specific Secrets
+
+resource "databricks_secret" "user" {
+  key          = "user"
+  string_value = var.account_user
+  scope        = databricks_secret_scope.sat.id
+}
+
+resource "databricks_secret" "pass" {
+  key          = "pass"
+  string_value = var.account_pass
+  scope        = databricks_secret_scope.sat.id
+}
+
+### Azure Specific Secrets
+
+#resource "databricks_secret" "client-secret" {
+#  key          = "client-secret"
+#  string_value = var.client_secret
+#  scope        = databricks_secret_scope.sat.id
+#}
+#
+#resource "databricks_secret" "subscription-id" {
+#  key          = "subscription-id"
+#  string_value = var.subscription_id
+#  scope        = databricks_secret_scope.sat.id
+#}
+#
+#resource "databricks_secret" "tenant-id" {
+#  key          = "tenant-id"
+#  string_value = var.tenant_id
+#  scope        = databricks_secret_scope.sat.id
+#}
+#
+#resource "databricks_secret" "client-id" {
+#  key          = "client-id"
+#  string_value = var.client_id
+#  scope        = databricks_secret_scope.sat.id
+#}
