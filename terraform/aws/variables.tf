79b1e3dbf3ca0b4f01b8e2a290bd6d5210f5bd44
Antonio Irizarry <55394816+airizarryDB@users.noreply.github.com>
2023-02-28 12:22:21 -0500

    Update variables.tf

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -10,6 +10,21 @@ variable "workspace_PAT" {
   description = "PAT should look like dapixxxxxxxxxxxxxxxxxxxx"
 }
 
+variable "account_console_id" {
+  description = "Databricks Account Console ID"
+}
+
+variable "sqlw_id" {
+  type = string
+  description = "16 character SQL Warehouse ID: Type new to have one created or enter an existing SQL Warehouse ID"
+  validation {
+      condition     = can(regex("^(new|[a-f0-9]{16})$", var.sqlw_id))
+      error_message = "Format 16 characters (0-9 and a-f). For more details reference: https://docs.databricks.com/administration-guide/account-api/iam-role.html"
+    }
+}
+
+### AWS Specific Variables
+
 variable "account_user" {
   description = "Account Console Username"
 }
@@ -17,6 +32,20 @@ variable "account_pass" {
   description = "Account Console Password"
 }
 
-variable "account_console_id" {
-  description = "Databricks Account Console ID"
-}
+### Azure Specific Variables
+
+#variable "client_id" {
+#  description = "Service Principal Application (client) ID"
+#}
+#
+#variable "tenant_id" {
+#  description = "The Directory (tenant) ID for the application registered in Azure AD"
+#}
+#
+#variable "subscription_id" {
+#  description = "Azure subscriptionId"
+#}
+#
+#variable "client_secret" {
+#  description = "SP Secret"
+#}
