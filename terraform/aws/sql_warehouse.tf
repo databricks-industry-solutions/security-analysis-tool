727524684a19f194d677887e82786408fb9f33a7
Antonio Irizarry <55394816+airizarryDB@users.noreply.github.com>
2023-02-28 12:24:22 -0500

    Update sql_warehouse.tf

--- terraform/aws/sql_warehouse.tf
+++ terraform/aws/sql_warehouse.tf
@@ -1,6 +1,9 @@
-resource "databricks_sql_endpoint" "this" {
+resource "databricks_sql_endpoint" "new" {
+  count            = var.sqlw_id == "new" ? 1 : 0
   name             = "SAT Warehouse"
   cluster_size     = "Small"
+  #If serverless is available uncomment this next line
+  #enable_serverless_compute = true
   max_num_clusters = 1
 
   tags {
@@ -9,4 +12,9 @@ resource "databricks_sql_endpoint" "this" {
       value   = data.databricks_current_user.me.user_name
     }
   }
+}
+
+data "databricks_sql_warehouse" "old" {
+  count            = var.sqlw_id == "new" ? 0 : 1
+  id = var.sqlw_id
 }
