43dd14ded7381167f135173bcfdd55803f2a2065
Antonio Irizarry <55394816+airizarryDB@users.noreply.github.com>
2023-02-28 12:28:07 -0500

    Update data.tf

--- terraform/aws/data.tf
+++ terraform/aws/data.tf
@@ -4,6 +4,8 @@ data "databricks_node_type" "smallest" {
   local_disk = true
   min_cores   = 4
   gb_per_core = 8
+  photon_worker_capable = true
+  photon_driver_capable = true
 }
 
 data "databricks_spark_version" "latest_lts" {
