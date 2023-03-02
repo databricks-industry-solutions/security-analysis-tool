f1e1a4b83637bda5d4044b1b3455b23f5e2ded06
Antonio Irizarry <55394816+airizarryDB@users.noreply.github.com>
2023-03-02 14:24:45 -0500

    Update repo.tf

--- terraform/gcp/repo.tf
+++ terraform/gcp/repo.tf
@@ -1,5 +1,5 @@
 #Make sure Files in Repos option is enabled in Workspace Admin Console > Workspace Settings
 
 resource "databricks_repo" "security-analysis-tool" {
-  url = "https://github.com/airizarryDB/security-analysis-tool.git" # "https://github.com/databricks-industry-solutions/security-analysis-tool.git"
+  url = "https://github.com/databricks-industry-solutions/security-analysis-tool.git"
 }
