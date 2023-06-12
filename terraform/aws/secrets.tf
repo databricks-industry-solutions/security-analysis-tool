8cbb0178b34183dafcf68d5a94e016e287f3738b
Arun Pamulapati <39059536+arunpamulapati@users.noreply.github.com>
2023-06-12 13:03:16 -0400

    Update secrets.tf
    changed to use - for keys for consistency

--- terraform/aws/secrets.tf
+++ terraform/aws/secrets.tf
@@ -13,19 +13,19 @@ resource "databricks_secret" "pass" {
 }
 
 resource "databricks_secret" "use_sp_auth" {
-  key          = "use_sp_auth"
+  key          = "use-sp-auth"
   string_value = var.use_sp_auth
   scope        = module.common.secret_scope_id
 }
 
 resource "databricks_secret" "client_id" {
-  key          = "client_id"
+  key          = "client-id"
   string_value = var.client_id
   scope        = module.common.secret_scope_id
 }
 
 resource "databricks_secret" "client_secret" {
-  key          = "client_secret"
+  key          = "client-secret"
   string_value = var.client_secret
   scope        = module.common.secret_scope_id
 }
