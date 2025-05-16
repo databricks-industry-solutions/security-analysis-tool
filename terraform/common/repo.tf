583abbb11f2619982c1e65fab9984a68725a1463
David Leiva <david.leiva@databricks.com>
2025-05-16 09:04:49 -0600

    update databricks_repo to use tag "v0.3.4" instead of branch "main"

--- terraform/common/repo.tf
+++ terraform/common/repo.tf
@@ -2,6 +2,6 @@
 
 resource "databricks_repo" "security_analysis_tool" {
   url    = "https://github.com/databricks-industry-solutions/security-analysis-tool.git"
-  branch = "main"
+  tag    = "v0.3.4"
   path   = "/Applications/SAT_TF"
 }
