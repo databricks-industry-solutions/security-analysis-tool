965da41e870d0a8da7ae1850b26d2d200e2b578a
arunpamulapati <arunpamulapati>
2025-08-06 15:24:08 +0000

    Clean run on aws after these fixes

--- notebooks/security_analysis_driver.py
+++ notebooks/security_analysis_driver.py
@@ -140,7 +140,7 @@ def processWorkspace(wsrow):
     retstr = dbutils.notebook.run(
         f"{basePath()}/notebooks/Utils/workspace_bootstrap",
         3000,
-        {"json_": json.dumps(ws_json)},
+        {"json_": json.dumps(ws_json),"origin": "driver"},
     )
     if "Completed SAT" not in retstr:
         raise Exception("Workspace Bootstrap failed. Skipping workspace analysis")
