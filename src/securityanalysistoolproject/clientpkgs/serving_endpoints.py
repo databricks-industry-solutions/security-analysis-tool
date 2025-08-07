1e60244ed3c4cf6ef918a30891c321ed0548b487
Arun Pamulapati <39059536+arunpamulapati@users.noreply.github.com>
2025-08-07 10:53:11 -0400

    Cleanup and gov cloud diagnostics (#238)
    Co-authored-by: arunpamulapati <arunpamulapati>

--- src/securityanalysistoolproject/clientpkgs/serving_endpoints.py
+++ src/securityanalysistoolproject/clientpkgs/serving_endpoints.py
@@ -6,20 +6,7 @@ class ServingEndpoints(SatDBClient):
     '''serving endpoints client helper'''
 
 
-    # permissions are implemented in the permissions_client.py
-    # def get_serving_permissions(self, serving_endpoint_id):
-    #     """
-    #     Returns endpoint permissions.
-    #     """
-    #     permissionlst= self.get(f"/permissions/serving-endpoints/{serving_endpoint_id}", version='2.0').get('access_control_list', [])
-    #     return permissionlst    
 
-    # def get_serving_permission_levels(self, serving_endpoint_id):
-    #     """
-    #     Returns endpoint permissions.
-    #     """
-    #     permissionlst= self.get(f"/permissions/serving-endpoints/{serving_endpoint_id}/permissionLevels", version='2.0').get('permission_levels', [])
-    #     return permissionlst    
 
 
 
