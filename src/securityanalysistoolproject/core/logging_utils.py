cdc05b1ec8f1151d5bb355ef802b2a082b5b091f
Tristan Jahnke <trjahnke@protonmail.com>
2026-02-17 16:11:19 -0600

    chore: format

--- src/securityanalysistoolproject/core/logging_utils.py
+++ src/securityanalysistoolproject/core/logging_utils.py
@@ -1,23 +1,27 @@
-'''logging module'''
+"""logging module"""
+
 import logging
 import os
-import sys
 import platform
-#from databricks.sdk.runtime import dbutils
+import sys
 
-class LoggingUtils():
-    '''Logging utils helper'''
+# from databricks.sdk.runtime import dbutils
+
+
+class LoggingUtils:
+    """Logging utils helper"""
+
     loglevel = logging.INFO
 
     @classmethod
     def set_logger_level(cls, loglevel_v):
-        '''set the logger level before calling get_logger'''
+        """set the logger level before calling get_logger"""
         cls.loglevel = loglevel_v
 
-    #DEBUG < INFO < WARNING < ERROR < CRITICAL
+    # DEBUG < INFO < WARNING < ERROR < CRITICAL
     @classmethod
-    def get_logger(cls, modname='_profiler_'):
-        '''get logger object'''
+    def get_logger(cls, modname="_profiler_"):
+        """get logger object"""
         log_base_dir = f"{cls.basePath()}/logs"
 
         if not os.path.isdir(log_base_dir):
@@ -27,9 +31,8 @@ class LoggingUtils():
         logpath = f"{log_base_dir}/sat.log"
 
         if not os.path.exists(logpath):
-            with open(logpath, 'w') as f:
+            with open(logpath, "w") as f:
                 f.write("")
-        
 
         # Create a custom logger
         logger = logging.getLogger(modname)
@@ -39,8 +42,12 @@ class LoggingUtils():
             c_handler = logging.StreamHandler(sys.stdout)
             f_handler = logging.FileHandler(logpath)
             # Create formatters and add it to handlers
-            c_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-            f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            c_format = logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            )
+            f_format = logging.Formatter(
+                "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            )
             c_handler.setFormatter(c_format)
             f_handler.setFormatter(f_format)
 
@@ -50,30 +57,33 @@ class LoggingUtils():
         logger.setLevel(cls.loglevel)
         return logger
 
-    #DEBUG < INFO < WARNING < ERROR < CRITICAL
+    # DEBUG < INFO < WARNING < ERROR < CRITICAL
     # pylint: disable=multiple-statements
     @staticmethod
     def get_log_level(vloglevel):
-        '''get log level that is set'''
+        """get log level that is set"""
         vloglevel = vloglevel.upper()
-        if vloglevel == "DEBUG": return logging.DEBUG
-        elif vloglevel == "INFO": return logging.INFO
-        elif vloglevel == "WARNING": return logging.WARNING
-        elif vloglevel == "ERROR": return logging.ERROR
-        elif vloglevel == "CRITICAL": return logging.CRITICAL
-    # pylint: enable=multiple-statements
-    
+        if vloglevel == "DEBUG":
+            return logging.DEBUG
+        elif vloglevel == "INFO":
+            return logging.INFO
+        elif vloglevel == "WARNING":
+            return logging.WARNING
+        elif vloglevel == "ERROR":
+            return logging.ERROR
+        elif vloglevel == "CRITICAL":
+            return logging.CRITICAL
 
+    # pylint: enable=multiple-statements
 
     @staticmethod
     def basePath():
         path = os.getcwd()
-        ind=path.find("/notebooks")
-        if ind==-1:
-            return("~/temp")
-        path = path[: ind]
-        if path.startswith('/Workspace'):
-            return(f'{path}')
+        ind = path.find("/notebooks")
+        if ind == -1:
+            return "~/temp"
+        path = path[:ind]
+        if path.startswith("/Workspace"):
+            return f"{path}"
         return f"/Workspace{path}"
-    
- 
+
